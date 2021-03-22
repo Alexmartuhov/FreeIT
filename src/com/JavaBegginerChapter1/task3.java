5c13c2809e963244dfa9f81f64bec885f97fabf2
a.martuhov <alexmartuhov@gmail.com>
2021-03-22 23:13:42 +0300

    JavaChapter1 task 1-8

--- src/com/JavaBegginerChapter1/task3.java
+++ src/com/JavaBegginerChapter1/task3.java
@@ -7,6 +7,10 @@ public class task3 {
         int m=(s-sec)/60;
         int min=m%60;
         int h=(m-min)/60;
-        System.out.println(h+" час "+ min+" минут "+ sec+" секунд");
+        int hours=h%24;
+        int d=(h-hours)/24;
+        int days=d%7;
+        int w=(d-days)/7;
+        System.out.println(w+" недель "+days+" дней "+hours+" часов "+ min+" минут "+ sec+" секунд");
     }
 }
