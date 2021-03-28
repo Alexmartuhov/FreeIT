7fe8e730dc5ef1cbd8e391a406f4c4c6643d31c7
a.martuhov <alexmartuhov@gmail.com>
2021-03-28 12:33:57 +0300

    JBC1 Task9-10

--- src/com/JavaBegginerChapter1/task1.java
+++
@@ -1,7 +0,0 @@
-package com.JavaBegginerChapter1;
-
-public class task1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- src/com/JavaBegginerChapter1/task2.java
+++
@@ -1,7 +0,0 @@
-package com.JavaBegginerChapter1;
-
-public class task2 {
-    public static void main(String[] args) {
-        System.out.println("Hello, Alex!");
-    }
-}
